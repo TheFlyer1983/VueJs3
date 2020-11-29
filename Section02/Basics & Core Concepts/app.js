9a988489a2f16c09093910291a0f38952b0d0942
TheFlyer1983 <theflyer1983@gmail.com>
2020-11-29 17:16:02 +0000

    Finishing the basics with v-html

--- Section02/Basics & Core Concepts/app.js
+++ Section02/Basics & Core Concepts/app.js
@@ -1,17 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: '<h3>Master Vue and build amazing apps!</h3>',
       vueLink: 'https://vuejs.org',
-    }
+    };
   },
   methods: {
     outputGoal() {
       const randomNumber = Math.random();
       if (randomNumber < 0.5) {
-        return 'Learn Vue!';
+        return this.courseGoalA;
       } else {
-        return 'Master Vue!';
+        return this.courseGoalB;
       }
     },
   },
