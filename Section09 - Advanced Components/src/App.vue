622ea0fc8f317da5c93f729465631d30c847bfc9
TheFlyer1983 <theflyer1983@gmail.com>
2020-12-09 21:19:47 +0000

    Dynamic Components and Teleporting Elements

--- Section09 - Advanced Components/src/App.vue
+++ Section09 - Advanced Components/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -10,31 +10,48 @@
     <course-goals #default="slotProps">
       <h2>{{ slotProps.item }}</h2>
       <p>{{ slotProps.anotherProp }}</p>
-    </course-goals>
+    </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
   import TheHeader from './components/TheHeader.vue';
-  import BadgeList from './components/BadgeList.vue';
-  import UserInfo from './components/UserInfo.vue';
-  import CourseGoals from './components/CourseGoals.vue';
+  // import BadgeList from './components/BadgeList.vue';
+  // import UserInfo from './components/UserInfo.vue';
+  // import CourseGoals from './components/CourseGoals.vue';
+  import ActiveGoals from './components/ActiveGoals.vue';
+  import ManageGoals from './components/ManageGoals.vue';
 
   export default {
     components: {
       TheHeader,
-      BadgeList,
-      UserInfo,
-      CourseGoals,
+      // BadgeList,
+      // UserInfo,
+      // CourseGoals,
+      ActiveGoals,
+      ManageGoals,
     },
     data() {
       return {
+        selectedComponent: 'active-goals',
         activeUser: {
           name: 'Paul Livingstone',
           description: 'Site owner and admin',
           role: 'admin',
         },
       };
+    },
+    methods: {
+      setSelectedComponent(cmp) {
+        this.selectedComponent = cmp;
+      },
     },
   };
 </script>
