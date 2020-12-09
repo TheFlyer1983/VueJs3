4fb4586f5399d0662102d2a1257cf3e0c51d3b11
Paul Livingstone <36920884+TheFlyer1983@users.noreply.github.com>
2020-12-09 12:01:53 +0000

    Section08 (#8)

---
+++ Section08 - Component Communication/Assignment06/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <section>
+    <header>
+      <h1>Assignment 06</h1>
+    </header>
+    <div class="components">
+      <div class="component">
+        <h1>Active User</h1>
+        <active-user :user="user"></active-user>
+      </div>
+      <div class="component">
+        <h1>User Data</h1>
+        <user-data @update-user="updateUser"></user-data>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import ActiveUser from './components/ActiveUser.vue';
+  import UserData from './components/UserData.vue';
+
+  export default {
+    components: {
+      'active-user': ActiveUser,
+      'user-data': UserData,
+    },
+    data() {
+      return {
+        user: {
+          name: 'Paul',
+          age: 37,
+        },
+      };
+    },
+    methods: {
+      updateUser(user) {
+        this.user.name = user.name;
+        this.user.age = +user.age;
+      },
+    },
+  };
+</script>
+
+<style>
+  body {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  header {
+    background-color: lightcoral;
+    padding: 1rem;
+  }
+  header h1 {
+    font-size: 3rem;
+  }
+  h1 {
+    text-align: center;
+  }
+  .components {
+    display: flex;
+    justify-content: space-evenly;
+  }
+</style>
---
+++ Section08 - Component Communication/Assignment06/src/components/ActiveUser.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user-data">
+    <h2>Name: {{ user.name }}</h2>
+    <h3>Age: {{ user.age }}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .user-data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    background-color: lightgreen;
+    padding: 2rem;
+  }
+
+  /* .user-data h2,
+  .user-data h3 {
+    
+  } */
+</style>
