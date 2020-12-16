df4e9a4ccd2c0dc42352c947014fb43b2ff59d8a
TheFlyer1983 <theflyer1983@gmail.com>
2020-12-16 10:44:50 +0000

    Transitions

---
+++ Section14 - Transitions/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue';
+import router from './router';
+
+import App from './App.vue';
+import BaseModal from './components/BaseModal.vue';
+
+const app = createApp(App);
+
+app.component('base-modal', BaseModal);
+
+app.use(router);
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
