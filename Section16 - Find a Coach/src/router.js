3b3bbe43cd0e2fc49a3215c82f19bbe57b4d16a2
TheFlyer1983 <theflyer1983@gmail.com>
2020-12-18 13:53:48 +0000

    Blank pages, and routes.

---
+++ Section16 - Find a Coach/src/router.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/coaches',
+  },
+  {
+    path: '/coaches',
+    component: () => import('./pages/coaches/CoachesList.vue'),
+  },
+  {
+    path: '/coaches/:id',
+    component: () => import('./pages/coaches/CoachesDetail.vue'),
+    children: {
+      path: 'contact',
+      component: () => import('./pages/requests/ContactCoach.vue'),
+    },
+  },
+  {
+    path: '/register',
+    component: () => import('./pages/coaches/CoachRegistration.vue'),
+  },
+  {
+    path: '/requests',
+    component: () => import('./pages/requests/RequestsReceived.vue'),
+  },
+  {
+    path: '/:notFound(.*)',
+    component: () => import('./pages/NotFound.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory,
+  routes,
+});
+
+export default router;
