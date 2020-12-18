06ac08f8cea271a21699368091e08e2e7e0794f0
TheFlyer1983 <theflyer1983@gmail.com>
2020-12-18 20:48:12 +0000

    Coach Registration Form

--- Section16 - Find a Coach/src/pages/coaches/CoachRegistration.vue
+++ Section16 - Find a Coach/src/pages/coaches/CoachRegistration.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>REGISTER</div>
+  <div>
+    <section>
+      <base-card>
+        <h2>Register as a coach now!</h2>
+        <coach-form @save-data="saveData"></coach-form>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
-  export default {};
+  import CoachForm from '../../components/coaches/CoachForm.vue';
+  export default {
+    components: {
+      CoachForm,
+    },
+    methods: {
+      saveData(data) {
+        this.$store.dispatch('coaches/registerCoach', data);
+        this.$router.replace('/coaches');
+      },
+    },
+  };
 </script>
 
 <style></style>
