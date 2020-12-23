66388efaa178accf9365412f925582881b169a14
TheFlyer1983 <theflyer1983@gmail.com>
2020-12-23 10:05:40 +0000

    Added loading spinner

--- Section16 - Find a Coach/src/pages/coaches/CoachesList.vue
+++ Section16 - Find a Coach/src/pages/coaches/CoachesList.vue
@@ -7,9 +7,12 @@
       <base-card>
         <div class="controls">
           <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-          <base-button link to="/register" v-if="!isCoach">Register as Coach</base-button>
+          <base-button link to="/register" v-if="!isCoach && !isLoading">Register as Coach</base-button>
         </div>
-        <ul v-if="hasCoaches">
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasCoaches">
           <coach-item
             v-for="coach in filteredCoaches"
             :key="coach.id"
@@ -37,6 +40,7 @@
     },
     data() {
       return {
+        isLoading: false,
         activeFilters: {
           frontend: true,
           backend: true,
@@ -64,7 +68,7 @@
         });
       },
       hasCoaches() {
-        return this.$store.getters['coaches/hasCoaches'];
+        return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
       },
     },
     created() {
@@ -74,8 +78,10 @@
       setFilters(updatedFilters) {
         this.activeFilters = updatedFilters;
       },
-      loadCoaches() {
-        this.$store.dispatch('coaches/loadCoaches');
+      async loadCoaches() {
+        this.isLoading = true;
+        await this.$store.dispatch('coaches/loadCoaches');
+        this.isLoading = false;
       }
     },
   };
