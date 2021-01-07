98e0da21b31090a24172d68eadfc24fddb9fa1aa
TheFlyer1983 <theflyer1983@gmail.com>
2021-01-07 15:37:07 +0000

    Local and global mixins

--- Section20/Mixins/src/components/DeleteUser.vue
+++ Section20/Mixins/src/components/DeleteUser.vue
@@ -1,32 +1,28 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
-    <p>Do you want to continue with deleting a user?</p>
-  </user-alert>
-  <section>
-    <h2>Delete a User</h2>
-    <button @click="showAlert">Delete User</button>
-  </section>
+  <div>
+    <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+      <p>Do you want to continue with deleting a user?</p>
+    </user-alert>
+    <section>
+      <h2>Delete a User</h2>
+      <button @click="showAlert">Delete User</button>
+    </section>
+  </div>
 </template>
 
 <script>
-import UserAlert from './UserAlert.vue';
+  import UserAlert from './UserAlert.vue';
+  import alertMixin from '../mixins/alert.js';
 
-export default {
-  components: {
-    UserAlert,
-  },
-  data() {
-    return {
-      alertIsVisible: false,
-    };
-  },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
+  export default {
+    components: {
+      UserAlert,
     },
-    hideAlert() {
-      this.alertIsVisible = false;
+    data() {
+      return {
+        alertTitle: 'Delete User',
+      }
     },
-  },
-};
+    mixins: [alertMixin],
+  };
 </script>
