0840a0633df220288f39cc8ebbda054740a2360a
TheFlyer1983 <theflyer1983@gmail.com>
2021-01-07 13:53:17 +0000

    Migration complete

--- Section19/Migrating to Composition API/src/components/users/UserList.vue
+++ Section19/Migrating to Composition API/src/components/users/UserList.vue
@@ -3,8 +3,10 @@
     <h2>Active Users</h2>
     <base-search @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
     <div>
-      <button @click="sort('asc')" :class="{selected: sorting === 'asc'}">Sort Ascending</button>
-      <button @click="sort('desc')" :class="{selected: sorting === 'desc'}">Sort Descending</button>
+      <button @click="sort('asc')" :class="{ selected: sorting === 'asc' }">Sort Ascending</button>
+      <button @click="sort('desc')" :class="{ selected: sorting === 'desc' }">
+        Sort Descending
+      </button>
     </div>
     <ul>
       <user-item
@@ -19,73 +21,132 @@
 </template>
 
 <script>
-import UserItem from './UserItem.vue';
+  import { ref, computed, watch } from 'vue';
+  import UserItem from './UserItem.vue';
 
-export default {
-  components: {
-    UserItem,
-  },
-  props: ['users'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-      sorting: null,
-    };
-  },
-  computed: {
-    availableUsers() {
-      let users = [];
-      if (this.activeSearchTerm) {
-        users = this.users.filter((usr) =>
-          usr.fullName.includes(this.activeSearchTerm)
-        );
-      } else if (this.users) {
-        users = this.users;
-      }
-      return users;
+  export default {
+    components: {
+      UserItem,
     },
-    displayedUsers() {
-      if (!this.sorting) {
-        return this.availableUsers;
-      }
-      return this.availableUsers.slice().sort((u1, u2) => {
-        if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (this.sorting === 'asc') {
-          return -1;
-        } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
+    props: ['users'],
+    emits: ['list-projects'],
+    setup(props) {
+      const enteredSearchTerm = ref('');
+      const activeSearchTerm = ref('');
+
+      const availableUsers = computed(() => {
+        let users = [];
+        if (activeSearchTerm.value) {
+          users = props.users.filter(usr => usr.fullName.includes(activeSearchTerm.value));
+        } else if (props.users) {
+          users = props.users;
         }
+        return users;
       });
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-    sort(mode) {
-      this.sorting = mode;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
+
+      watch(enteredSearchTerm, (newValue) => {
+        setTimeout(() => {
+          if (newValue === enteredSearchTerm.value) {
+            activeSearchTerm.value = newValue;
+          }
+        }, 300);
+      })
+
+      function updateSearch(val) {
+        enteredSearchTerm.value = val;
+      }
+
+      const sorting = ref(null);
+
+      const displayedUsers = computed(() => {
+        if (!sorting.value) {
+          return availableUsers.value;
         }
-      }, 300);
-    }
-  },
-};
+        return availableUsers.value.slice().sort((u1, u2) => {
+          if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+            return 1;
+          } else if (sorting.value === 'asc') {
+            return -1;
+          } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+      });
+
+      function sort(mode) {
+        sorting.value = mode;
+      }
+
+      return {
+        enteredSearchTerm,
+        activeSearchTerm,
+        availableUsers,
+        updateSearch,
+        sorting,
+        displayedUsers,
+        sort,
+      };
+    },
+    // data() {
+    //   return {
+    //     enteredSearchTerm: '',
+    //     activeSearchTerm: '',
+    //     sorting: null,
+    //   };
+    // },
+    // computed: {
+    //   availableUsers() {
+    //     let users = [];
+    //     if (this.activeSearchTerm) {
+    //       users = this.users.filter(usr => usr.fullName.includes(this.activeSearchTerm));
+    //     } else if (this.users) {
+    //       users = this.users;
+    //     }
+    //     return users;
+    //   },
+    //   displayedUsers() {
+    //     if (!this.sorting) {
+    //       return this.availableUsers;
+    //     }
+    //     return this.availableUsers.slice().sort((u1, u2) => {
+    //       if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
+    //         return 1;
+    //       } else if (this.sorting === 'asc') {
+    //         return -1;
+    //       } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
+    //         return -1;
+    //       } else {
+    //         return 1;
+    //       }
+    //     });
+    //   },
+    // },
+    // methods: {
+    //   updateSearch(val) {
+    //     this.enteredSearchTerm = val;
+    //   },
+    //   sort(mode) {
+    //     this.sorting = mode;
+    //   },
+    // },
+    // watch: {
+    //   enteredSearchTerm(val) {
+    //     setTimeout(() => {
+    //       if (val === this.enteredSearchTerm) {
+    //         this.activeSearchTerm = val;
+    //       }
+    //     }, 300);
+    //   },
+    // },
+  };
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
 </style>
