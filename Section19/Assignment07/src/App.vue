c2fa23e36d887f532293b9ea2401e31bdcca9660
Paul Livingstone <36920884+TheFlyer1983@users.noreply.github.com>
2021-01-07 14:51:14 +0000

    section19 (#19)

---
+++ Section19/Assignment07/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h2>My Course Goal</h2>
+    <!-- Task 1: Output your main course goal with help of the composition API -->
+    <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+    <h3 v-if="data.showGoal">{{ data.goal }}</h3>
+    <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+    <button @click="toggleGoal">Toggle Goal</button>
+    <!-- Task 3: Manage data in three ways -->
+    <!-- => Separate refs -->
+    <!-- => Ref Object -->
+    <!-- => Reactive Object -->
+    <!-- Task 4: Also solve the assignment with the Options API -->
+  </div>
+</template>
+
+<script>
+  // import { ref } from 'vue';
+  import { reactive } from 'vue';
+
+  export default {
+    // Composition API
+    setup() {
+      // Separate Refs
+      // const goal = ref('Learn the Composition API');
+      // const showGoal = ref(true);
+
+      // Ref Object
+      // const data = ref({
+      //   goal: 'Learn the CompositionAPI',
+      //   showGoal: true,
+      // });
+      // Reactive Object
+      const data = reactive({
+        goal: 'Learn the CompositionAPI',
+        showGoal: true,
+      });
+
+      function toggleGoal() {
+        // Separate Refs
+        // showGoal.value = !showGoal.value;
+
+        // Ref object
+        // data.value.showGoal = !data.value.showGoal;
+        
+        // Reactive Object
+        data.showGoal = !data.showGoal;
+      }
+
+      return {
+        // Separate Refs
+        // goal,
+        // showGoal,
+        toggleGoal,
+        data,
+      };
+    },
+
+    // Options API
+    /*data() {
+      return {
+        goal: 'Learn the Composition API',
+        showGoal: true,
+      };
+    },
+    methods: {
+      toggleGoal() {
+        this.showGoal = !this.showGoal;
+      },
+    },*/
+  };
+</script>
+
+<style>
+  html {
+    font-family: sans-serif;
+  }
+  body {
+    margin: 3rem;
+    text-align: center;
+  }
+</style>
---
+++ Section19/Composition API/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <section class="container">
+    <user-data :first-name="firstName" :last-name="lastName" class="test"></user-data>
+    <button @click="setNewAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <!-- <input type="text" placeholder="Last Name" v-model="lastName" /> -->
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
+    </div>
+  </section>
+</template>
+
+<script>
+  import { ref, /*reactive,*/ computed, watch, provide } from 'vue';
+  import UserData from './components/UserData.vue';
+
+  export default {
+    components: {
+      UserData,
+    },
+    setup() {
+      const uAge = ref(37);
+      const firstName = ref('');
+      const lastName = ref('');
+      const lastNameInput = ref(null);
+      // const user = reactive({
+      //   name: 'Paul',
+      //   age: 37,
+      // });
+
+      provide('userAge', uAge);
+
+      const uName = computed(() => {
+        return firstName.value + ' ' + lastName.value;
+      });
+
+      watch([uAge, uName], (newvalue, oldvalue) => {
+        console.log('Old age:', oldvalue[0]);
+        console.log('New age:', newvalue[0]);
+      });
+
+      function setNewAge() {
+        uAge.value = 38;
+      }
+
+      function setLastName() {
+        lastName.value = lastNameInput.value.value;
+      }
+
+      // function setFirstName(event) {
+      //   firstName.value = event.target.value;
+      // }
+
+      // function setLastName(event) {
+      //   lastName.value = event.target.value;
+      // }
+
+      // setTimeout(() => {
+      //   user.name = 'Tall Paul';
+      // }, 2000);
+
+      return {
+        uName,
+        uAge,
+        setNewAge,
+        firstName,
+        lastName,
+        lastNameInput,
+        setLastName,
+      };
+    },
+    // data() {
+    //   return {
+    //     userName: 'Maximilian',
+    //     age: 38
+    //   };
+    // },
+    // methods: {
+    //   setNewAge() {
+    //     this.age = 38
+    //   }
+    // }
+  };
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  .container {
+    margin: 3rem auto;
+    max-width: 30rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+    text-align: center;
+  }
+</style>
