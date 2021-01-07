627f4dcd254c188be41159563ea70a92ef13b94a
TheFlyer1983 <theflyer1983@gmail.com>
2021-01-07 11:49:16 +0000

    Lifecycle Hooks in the CompositionAPI

--- Section19/Composition API/src/components/UserData.vue
+++ Section19/Composition API/src/components/UserData.vue
@@ -6,7 +6,17 @@
 </template>
 
 <script>
-  import { computed, inject } from 'vue';
+  import {
+    computed,
+    inject,
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted,
+  } from 'vue';
+
   export default {
     props: ['firstName', 'lastName'],
     setup(props, context) {
@@ -16,6 +26,25 @@
       const age = inject('userAge');
 
       console.log(context);
+
+      onBeforeMount(() => {
+        console.log('onBeforeMount');
+      });
+      onMounted(() => {
+        console.log('onMounted');
+      });
+      onBeforeUpdate(() => {
+        console.log('onBeforeUpdate');
+      });
+      onUpdated(() => {
+        console.log('onUpdated');
+      });
+      onBeforeUnmount(() => {
+        console.log('onBeforeUnmount');
+      });
+      onUnmounted(() => {
+        console.log('onUnmounted');
+      });
 
       // context.emit('save-data', 1); // this.$emit('save-data', 1);
 
