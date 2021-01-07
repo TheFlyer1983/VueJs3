0d067f717df17179dfee864613958666216f87cc
TheFlyer1983 <theflyer1983@gmail.com>
2021-01-07 16:03:57 +0000

    Custom Compositions or hooks

--- Section20/Custom Compositions/src/components/DeleteUser.vue
+++ Section20/Custom Compositions/src/components/DeleteUser.vue
@@ -1,36 +1,35 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
-    <p>Do you want to continue with deleting a user?</p>
-  </user-alert>
-  <section>
-    <h2>Delete a User</h2>
-    <button @click="showAlert">Delete User</button>
-  </section>
+  <div>
+    <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+      <p>Do you want to continue with deleting a user?</p>
+    </user-alert>
+    <section>
+      <h2>Delete a User</h2>
+      <button @click="showAlert">Delete User</button>
+    </section>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import UserAlert from './UserAlert.vue';
+  import { ref } from 'vue';
 
-export default {
-  components: {
-    UserAlert,
-  },
-  setup() {
-    const alertIsVisible = ref(false);
+  import UserAlert from './UserAlert.vue';
+  import useAlert from '../hooks/alert.js';
 
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
+  export default {
+    components: {
+      UserAlert,
+    },
+    setup() {
+      const alertTitle = ref('Delete User?');
+      const [alertIsVisible, showAlert, hideAlert] = useAlert(true);
 
-    return {
-      alertIsVisible,
-      showAlert,
-      hideAlert,
-    };
-  },
-};
+      return {
+        alertIsVisible,
+        showAlert,
+        hideAlert,
+        alertTitle,
+      };
+    },
+  };
 </script>
