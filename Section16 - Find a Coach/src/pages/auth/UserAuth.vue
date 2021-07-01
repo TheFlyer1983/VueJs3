d0cc0a85572e508a8ff3153c95c9058400357f44
TheFlyer1983 <theflyer1983@gmail.com>
2021-07-01 17:08:08 +0100

    Added Vuelidate for better validation

--- Section16 - Find a Coach/src/pages/auth/UserAuth.vue
+++ Section16 - Find a Coach/src/pages/auth/UserAuth.vue
@@ -10,13 +10,13 @@
       <form @submit.prevent="submitForm">
         <div class="form-control">
           <label for="email">E-Mail</label>
-          <input type="text" id="email" v-model.trim="email" />
+          <input type="text" id="email" v-model.trim="loginForm.email" />
         </div>
         <div class="form-control">
           <label for="password">Password</label>
-          <input type="password" id="password" v-model.trim="password" />
+          <input type="password" id="password" v-model.trim="loginForm.password" />
         </div>
-        <p v-if="!formIsValid">
+        <p v-if="v$.$invalid && isFormSubmitted">
           Please enter a valid email and password (must be at least 6 characters long).
         </p>
         <base-button>{{ submitButtonCaption }}</base-button>
@@ -29,11 +29,14 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref, computed } from 'vue';
+  import { defineComponent, ref, reactive, computed } from 'vue';
   import { useStore } from 'vuex';
   import { useRouter, useRoute } from 'vue-router';
   // import { Login } from '@/types/interfaces';
   import { AuthActions } from '@/store/modules/auth/types';
+
+  import useVuelidate from '@vuelidate/core';
+  import { required, minLength, email } from '@vuelidate/validators';
 
   export default defineComponent({
     name: 'UserAuth',
@@ -42,12 +45,22 @@
       const router = useRouter();
       const route = useRoute();
 
-      const email = ref('');
-      const password = ref('');
-      const formIsValid = ref(true);
+      const loginForm = reactive({
+        email: '',
+        password: ''
+      })
+
+      const isFormSubmitted = ref(false);
       const mode = ref('login');
       const isLoading = ref(false);
       const error = ref(null);
+
+      const rules = {
+        email: {required, email},
+        password: {required, minLength: minLength(6)}
+      }
+
+      const v$ = useVuelidate(rules, loginForm)
 
       const submitButtonCaption = computed(() => {
         if (mode.value === 'login') {
@@ -66,17 +79,16 @@
       });
 
       async function submitForm(): Promise<void> {
-        formIsValid.value = true;
-        if (!email.value || !email.value.includes('@') || password.value.length < 6) {
-          formIsValid.value = false;
+        isFormSubmitted.value = true;
+        if (v$.value.$invalid) {
           return;
         }
 
         isLoading.value = true;
 
         const actionPayload = {
-          email: email.value,
-          password: password.value,
+          email: loginForm.email,
+          password: loginForm.password,
         };
         try {
           if (mode.value === 'login') {
@@ -105,9 +117,8 @@
       }
 
       return {
-        email,
-        password,
-        formIsValid,
+        loginForm,
+        isFormSubmitted,
         mode,
         isLoading,
         error,
@@ -116,6 +127,7 @@
         submitForm,
         switchAuthMode,
         handleError,
+        v$,
       };
     },
   });
