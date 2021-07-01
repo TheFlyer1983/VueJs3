759b373468db44dbd3a25144b3336598a49b1f50
Paul Livingstone <36920884+TheFlyer1983@users.noreply.github.com>
2021-07-01 17:15:21 +0100

    Added Vuelidate for better validation (#23)

--- Section16 - Find a Coach/src/components/coaches/CoachForm.vue
+++ Section16 - Find a Coach/src/components/coaches/CoachForm.vue
@@ -1,180 +1,95 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control" :class="{ invalid: !firstName.isValid }">
+    <div class="form-control" :class="{ invalid: v$.firstName.$invalid && isFormSubmitted }">
       <label for="firstname">First Name</label>
-      <input
-        type="text"
-        id="firstname"
-        v-model.trim="firstName.val"
-      />
-      <p v-if="!firstName.isValid">First Name must not be empty.</p>
+      <input type="text" id="firstname" v-model.trim="fields.firstName" />
+      <p v-if="v$.firstName.$invalid && isFormSubmitted">First Name must not be empty.</p>
     </div>
-    <div class="form-control" :class="{ invalid: !lastName.isValid }">
+    <div class="form-control" :class="{ invalid: v$.lastName.$invalid && isFormSubmitted }">
       <label for="lastname">Last Name</label>
-      <input
-        type="text"
-        id="lastname"
-        v-model.trim="lastName.val"
-      />
-      <p v-if="!lastName.isValid">Last Name must not be empty.</p>
+      <input type="text" id="lastname" v-model.trim="fields.lastName" />
+      <p v-if="v$.lastName.$invalid && isFormSubmitted">Last Name must not be empty.</p>
     </div>
-    <div class="form-control" :class="{ invalid: !description.isValid }">
+    <div class="form-control" :class="{ invalid: v$.description.$invalid && isFormSubmitted}">
       <label for="description">Description</label>
-      <textarea
-        id="description"
-        rows="5"
-        v-model.trim="description.val"
-      ></textarea>
-      <p v-if="!description.isValid">Description must not be empty.</p>
+      <textarea id="description" rows="5" v-model.trim="fields.description"></textarea>
+      <p v-if="v$.description.$invalid && isFormSubmitted">Description must not be empty.</p>
     </div>
-    <div class="form-control" :class="{ invalid: !rate.isValid }">
+    <div class="form-control" :class="{ invalid: v$.rate.$invalid && isFormSubmitted}">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" v-model.number="rate.val" />
-      <p v-if="!rate.isValid">Rate must be greater than 0.</p>
+      <input type="number" id="rate" v-model.number="fields.rate" />
+      <p v-if="v$.rate.$invalid && isFormSubmitted">Rate must be greater than 0.</p>
     </div>
-    <div class="form-control" :class="{ invalid: !areas.isValid }">
+    <div class="form-control" :class="{ invalid: v$.areas.$invalid && isFormSubmitted }">
       <h3>Areas of Expertise</h3>
       <div>
-        <input
-          type="checkbox"
-          id="frontend"
-          value="frontend"
-          v-model="areas.val"
-        />
+        <input type="checkbox" id="frontend" value="frontend" v-model="fields.areas" />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input
-          type="checkbox"
-          id="backend"
-          value="backend"
-          v-model="areas.val"
-        />
+        <input type="checkbox" id="backend" value="backend" v-model="fields.areas" />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input
-          type="checkbox"
-          id="career"
-          value="career"
-          v-model="areas.val"
-        />
+        <input type="checkbox" id="career" value="career" v-model="fields.areas" />
         <label for="carrer">Career Advisory</label>
       </div>
-      <p v-if="!areas.isValid">At least one expertise must be selected.</p>
+      <p v-if="v$.areas.$invalid && isFormSubmitted">At least one expertise must be selected.</p>
     </div>
-    <p v-if="!formIsValid">Please fix the above errors and submit again.</p>
+    <p v-if="v$.$invalid && isFormSubmitted">Please fix the above errors and submit again.</p>
     <base-button>Register</base-button>
   </form>
 </template>
 
 <script lang="ts">
-  import { defineComponent, reactive, ref, watch } from 'vue';
+  import { defineComponent, reactive, ref } from 'vue';
+  import useVuelidate from '@vuelidate/core';
+  import { required, minLength, minValue } from '@vuelidate/validators';
 
   export default defineComponent({
     name: 'CoachForm',
     emits: ['save-data'],
-    setup(props, context) {
-      const firstName = reactive({
-        val: '',
-        isValid: true,
+    setup(_props, context) {
+      const fields = reactive({
+        firstName: '',
+        lastName: '',
+        description: '',
+        rate: 0,
+        areas: [],
       });
-      const lastName = reactive({
-        val: '',
-        isValid: true,
-      });
-      const description = reactive({
-        val: '',
-        isValid: true,
-      });
-      const rate = reactive({
-        val: 0,
-        isValid: true,
-      });
-      const areas = reactive({
-        val: [],
-        isValid: true,
-      });
-      const formIsValid = ref(true);
 
-      function validateForm() {
-        formIsValid.value = true;
-        if (firstName.val === '') {
-          firstName.isValid = false;
-          formIsValid.value = false;
-        }
-        if (lastName.val === '') {
-          lastName.isValid = false;
-          formIsValid.value = false;
-        }
-        if (description.val === '') {
-          description.isValid = false;
-          formIsValid.value = false;
-        }
-        if (!rate.val || rate.val <= 0) {
-          rate.isValid = false;
-          formIsValid.value = false;
-        }
-        if (!areas.val.length) {
-          areas.isValid = false;
-          formIsValid.value = false;
-        }
-      }
+      const isFormSubmitted = ref(false)
 
-      watch(firstName, () => {
-        if (firstName.val !== ''){
-          firstName.isValid = true;
-        }
-      })
+      const rules = {
+        firstName: { required },
+        lastName: { required },
+        description: { required },
+        rate: { required, minValue: minValue(1) },
+        areas: { required, minLength: minLength(1) },
+      };
 
-      watch(lastName, () => {
-        if (lastName.val !== ''){
-          lastName.isValid = true;
-        }
-      })
-
-      watch(description, () => {
-        if (description.val !== ''){
-          description.isValid = true;
-        }
-      })
-
-      watch(rate, () => {
-        if (rate.val){
-          rate.isValid = true;
-        }
-      })
-
-      watch(areas, () => {
-        if (areas.val.length) {
-          areas.isValid = true;
-        }
-      })
+      const v$ = useVuelidate(rules, fields);
 
       function submitForm() {
-        validateForm();
-        if (!formIsValid.value) {
+        isFormSubmitted.value = true;
+        if (v$.value.$invalid) {
           return;
         }
         const formData = {
-          first: firstName.val,
-          last: lastName.val,
-          desc: description.val,
-          rate: rate.val,
-          areas: areas.val,
+          first: fields.firstName,
+          last: fields.lastName,
+          desc: fields.description,
+          rate: fields.rate,
+          areas: fields.areas,
         };
         context.emit('save-data', formData);
       }
 
       return {
-        firstName,
-        lastName,
-        description,
-        rate,
-        areas,
-        formIsValid,
-        validateForm,
+        fields,
         submitForm,
+        isFormSubmitted,
+        v$,
       };
     },
   });
